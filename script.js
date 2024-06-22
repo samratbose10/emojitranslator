4670bce242605710d2485e07bf0d52bea7b1f87f
Samrat Bose <samratofficial2009@gmail.com>
2024-06-22 19:42:47 +0530

    final commit

--- script.js
+++ script.js
@@ -28,13 +28,32 @@ const emojiDictionary = {
     ' ': '⬜'
 };
 
+const reverseEmojiDictionary = Object.entries(emojiDictionary).reduce((obj, [key, value]) => {
+    obj[value] = key;
+    return obj;
+}, {});
+
 document.getElementById('encryptBtn').addEventListener('click', () => {
     document.getElementById('buttons').classList.add('hidden');
     document.getElementById('encryptSection').classList.remove('hidden');
 });
 
+document.getElementById('decryptBtn').addEventListener('click', () => {
+    document.getElementById('buttons').classList.add('hidden');
+    document.getElementById('decryptSection').classList.remove('hidden');
+});
+
 document.getElementById('startEncryptBtn').addEventListener('click', () => {
-    const inputText = document.getElementById('inputText').value;
+    const inputText = document.getElementById('inputTextEncrypt').value;
     const encryptedText = inputText.split('').map(char => emojiDictionary[char.toLowerCase()] || char).join('');
-    document.getElementById('outputText').innerText = encryptedText;
+    document.getElementById('outputTextEncrypt').innerText = encryptedText;
+});
+
+document.getElementById('startDecryptBtn').addEventListener('click', () => {
+    const inputText = document.getElementById('inputTextDecrypt').value;
+    let decryptedText = '';
+    for (let char of inputText) {
+        decryptedText += reverseEmojiDictionary[char] || char;
+    }
+    document.getElementById('outputTextDecrypt').innerText = decryptedText;
 });
